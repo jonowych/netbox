d50b3fb51b621822cfd417d16a6616f1d3b5b0fc
jonowych <jonowych@users.noreply.github.com>
2017-08-17 14:14:14 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,39 +13,33 @@ exec 2> >(tee -a /tmp/install.log >&2)
 
 sudo apt-get update 
 
-python_apps=$(echo "python3 python3-dev python3-pip")
-prerequisites=$(echo "libxml2-dev libxslt1-dev libffi-dev graphviz libpq-dev libssl-dev zlib1g-dev")
+core_apps=$(echo "postgresql python3 python3-dev python3-pip")
+dev_tools=$(echo "libxml2-dev libxslt1-dev libffi-dev graphviz libpq-dev libssl-dev zlib1g-dev")
 
-# Install python apps
+# Install core apps
 #
 echo && echo -e "$(tput setaf 6)Installing python3 Apps. They will take a few minutes.$(tput sgr0)"
-
-for a in $python_apps; do
+for a in $core_apps; do
      echo -e "$(tput setaf 6)Installing $a .... Please wait .... $(tput sgr0)"
      sudo apt-get -qq -y install $a
      echo $a has been installed
      echo
 done
-# Change python version to python3 with priority 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+echo && echo -e "$(tput setaf 6)-- End of core apps installation --$(tput sgr0)"
 
-# --- End of python tools installation
-
-# Install Prerequisites
+# Install development tools
 #
-echo && echo -e "$(tput setaf 6)Installing prerequisites. They will take a few minutes.$(tput sgr0)"
-
+echo && echo -e "$(tput setaf 6)Installing development tools. They will take a few minutes.$(tput sgr0)"
 for a in $prerequisites; do
      echo -e "$(tput setaf 6)Installing $a .... Please wait .... $(tput sgr0)"
      sudo apt-get -qq -y install $a
      echo $a has been installed
      echo
 done
-# --- End of Prerequisites installation
+echo && echo -e "$(tput setaf 6)-- End of development tools installation --$(tput sgr0)"
 
 # Download latest netbox code
 #
-
 netbox_ver=$(echo "2.1.3")
 if [ ! -f /tmp/v"$netbox_ver".tar.gz ] ; then 
   cd /tmp
@@ -61,11 +55,11 @@ pip3 install -r requirements.txt
 host=$(cat /etc/hostname)
 ip=$(cat /etc/network/interfaces | grep "iface enp0s3" -A 2 \
       | grep address | awk '{ print $2 }')
-sed -i 's/python/python3/' /opt/netbox/netbox/generate_secret_key.py
+sed -i 's/python$/python3/' /opt/netbox/netbox/generate_secret_key.py
 key=$(/opt/netbox/netbox/generate_secret_key.py)
 
 cat /opt/netbox/netbox/netbox/configuration.example.py | \
-  sed "s/^ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \['$host', '$ip'/" | \
+  sed "s/^ALLOWED_HOSTS = \[\]$/ALLOWED_HOSTS = \['$host', '$ip'\]/" | \
   sed "s/'USER': '/'USER': 'sydadmin/" | \
   sed "s/'USERNAME': '/'USERNAME': 'sydadmin/" | \
   sed "s/'PASSWORD': '/'PASSWORD': '67\.Epping/" | \
@@ -74,11 +68,10 @@ cat /opt/netbox/netbox/netbox/configuration.example.py | \
 sudo mv -f /tmp/configuration.py /opt/netbox/netbox/netbox/
 cat /opt/netbox/netbox/netbox/configuration.py
 
-# pip3 install napalm
+pip3 install napalm
 
 # cd /opt/netbox/netbox/
-# ./manage.py migrate
-# ./manage.py createsuperuser
-# ./manage.py collectstatic --no-input
-# ./manage.py loaddata initial_data
-
+# python3 ./manage.py migrate
+# python3 ./manage.py createsuperuser
+# python3 ./manage.py collectstatic --no-input
+# python3 ./manage.py loaddata initial_data
