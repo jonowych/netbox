1cb3dd021364bec096becbca89f374a9b74a24ef
jonowych <jonowych@users.noreply.github.com>
2018-02-20 16:33:07 +1100

    Update install-core.sh

--- install-core.sh
+++ install-core.sh
@@ -8,36 +8,24 @@ if [ ! "${USER}" = "root" ] ; then
 exec >  >(tee -a /tmp/install.log)
 exec 2> >(tee -a /tmp/install.log >&2)
 
-sudo apt-get update 
+apt-get update 
+ln /usr/bin/python3.5 /usr/local/bin/python
 
 # install pip3 version 9.0.1
-#
-echo -e $(tput setaf 6)"\nInstalling pip3 .... Please wait ...." $(tput sgr0)
-apt-get install -y python3-pip
-
-# Upgrade pip components
-#
-pip3_apps=$(echo "pip virtualenv")
-for a in $pip3_apps; do
-     echo $(tput setaf 6)
-     echo "Upgrading $a .... Please wait .... "$(tput sgr0)
-     sudo -H /usr/bin/pip3 install --upgrade $a
-done
-
 echo $(tput setaf 6)
-echo "!!-- End of pip3 installation --!!"$(tput sgr0)
+echo "Installing pip3 .... Please wait ...." $(tput sgr0)
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+echo $(tput setaf 6)"!!-- End of pip3 installation --!!" $(tput sgr0)
 
-# Install core apps
-#
+# Install Postgresql
 core_apps=$(echo "libpq-dev postgresql")
 for a in $core_apps; do
      echo $(tput setaf 6)
      echo "Installing $a .... Please wait .... "$(tput sgr0)
      sudo apt-get -qq -y install $a
+     echo $(tput setaf 6)"!!-- End of $a installation --!!" $(tput sgr0)
 done
-
-echo $(tput setaf 6)
-echo "!!-- End of core apps installation --!!"$(tput sgr0)
 
 systemctl start postgresql
 systemctl enable postgresql
